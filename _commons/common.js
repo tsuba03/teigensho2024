80fd592d3b9583da3abc20d24bf953cc769a8f6e
Tsubasa <148679981+tsuba03@users.noreply.github.com>
2025-05-16 12:56:37 +0900

    uploaded ver.

--- _commons/common.js
+++ _commons/common.js
@@ -1,6 +1,6 @@
 $(function() {
-  $("#header").load("/_header/header.html");
-  $("#footer").load("/_footer/footer.html");
+  $("#header").load("/gakuseichousa/2024/teigensho/_header/header.html");
+  $("#footer").load("/gakuseichousa/2024/teigensho/_footer/footer.html");
     const opinion = createHeadline("学生の意見");
     const analysis = createHeadline("現状分析");
     const suggestion = createHeadline("提言");
